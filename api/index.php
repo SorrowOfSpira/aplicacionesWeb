8f69d67d93ce9923d83596a2d6be48c7d6f3a9bd
Franco <francomatamala1@hotmail.com>
2026-04-20 10:41:46 -0300

    cambio en api/index.php

--- api/index.php
+++ api/index.php
@@ -1,2 +1,19 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Cargamos el autoloader de Composer
+require __DIR__ . '/../vendor/autoload.php';
+
+// Arrancamos Laravel (Bootstrap)
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
