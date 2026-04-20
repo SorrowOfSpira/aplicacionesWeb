0f89b5879bf6db84ab7087b636efe3ac73051a1a
ash <ashpanda64@gmail.com>
2026-04-20 17:29:47 -0300

    div

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,10 +20,7 @@
             <a href="{{ route('dashboard') }}" class="inline-block bg-[#D84315] text-white px-6 py-3 rounded-md font-semibold hover:bg-opacity-90 transition-all">
                 Ir al Dashboard
             </a>
-        </div>
-        <div class="text-center bg-[#FAFAF5] p-10 rounded-xl shadow-sm border border-[#E0E0E0] max-w-lg w-full">
             <h1 class="text-center font-bold text-[#1B5E20] mb-2 font-['Nunito']">Autores: Franco Matamala, Ash Pablovich</h1>
-            </a>
         </div>
     </body>
 </html>
